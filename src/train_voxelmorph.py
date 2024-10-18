de5fba8650e345664c2b9d7d7ed3b05e044fcc9f
Utku İpek <utkuipek@Utkus-MacBook-Air.fritz.box>
2024-10-18 11:34:36 +0200

    able to read config file

--- src/train_voxelmorph.py
+++ src/train_voxelmorph.py
@@ -63,7 +63,14 @@ def combine_images(moving, fixed, moved):
 
 if __name__ == '__main__':
 
-    config_path = 'config.ini'
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Go up one level to the project root
+    project_root = os.path.dirname(current_dir)
+
+    # Construct the path to the config file
+    config_path = os.path.join(project_root, 'config.ini')
+
     config = configparser.ConfigParser()
     config.read(config_path)
 
